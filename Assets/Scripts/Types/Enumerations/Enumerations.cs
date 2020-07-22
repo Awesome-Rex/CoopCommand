7bdf40a16d8bdd15a66c8111e0d8c443a0ceaabf
Awesome-Rex <john.esqqq@gmail.com>
2020-07-21 20:55:47 -0400

    Before adding packages

--- Assets/Scripts/Types/Enumerations/Enumerations.cs
+++
@@ -1,5 +0,0 @@
-﻿public enum Curve { Linear, Interpolate }
-public enum Link { Offset, Match }
-public enum LocalRelativity { Natural, Constraint }
-
-public enum SnapDetectionMethod { Any, Staight, Custom }
